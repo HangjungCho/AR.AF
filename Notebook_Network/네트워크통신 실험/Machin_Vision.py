8b1dc0b55a59eeb4dd56fd0a7ac54bf8d508ee30
항정 조 <stneowh@naver.com>
2020-09-19 16:52:05 +0900

    모델추가

--- Notebook_Network/네트워크통신 실험/Machin_Vision.py
+++ Notebook_Network/네트워크통신 실험/Machin_Vision.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 
 import tensorflow as tf
 import cv2
@@ -16,60 +5,59 @@ import numpy as np
 import math
 from tensorflow.keras.models import load_model
 
-def process(img_input):
-
-    gray = cv2.cvtColor(img_input, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(gray, (28, 28), interpolation=cv2.INTER_AREA)
-    (thresh, img_binary) = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-
-
-    h,w = img_binary.shape
-
-  
-    ratio = 100/h
-    new_h = 100
-    new_w = w * ratio
-
-    img_empty = np.zeros((110,110), dtype=img_binary.dtype)
-    img_binary = cv2.resize(img_binary, (int(new_w), int(new_h)), interpolation=cv2.INTER_AREA)
-    img_empty[:img_binary.shape[0], :img_binary.shape[1]] = img_binary
-
-    img_binary = img_empty
-
-
-    cnts = cv2.findContours(img_binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-
-    # 컨투어의 무게중심 좌표를 구합니다. 
-    M = cv2.moments(cnts[0][0])
-    print('{}, {}, {}'.format(M["m00"],M["m10"],M["m01"]))
-    center_x = (M["m10"] / M["m00"])
-    center_y = (M["m01"] / M["m00"])
-
-    # 무게 중심이 이미지 중심으로 오도록 이동시킵니다. 
-    height,width = img_binary.shape[:2]
-    shiftx = width/2-center_x
-    shifty = height/2-center_y
-
-    Translation_Matrix = np.float32([[1, 0, shiftx],[0, 1, shifty]])
-    img_binary = cv2.warpAffine(img_binary, Translation_Matrix, (width,height))
-
-
-    img_binary = cv2.resize(img_binary, (28, 28), interpolation=cv2.INTER_AREA)
-    flatten = img_binary.flatten() / 255.0
-
-    return flatten
 
 
 
-# model = tf.keras.models.Sequential([
-#   tf.keras.layers.Flatten(),
-#   tf.keras.layers.Dense(512, activation=tf.nn.relu),
-#   tf.keras.layers.Dropout(0.2),
-#   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-# ])
+class MachinVisionProcess( Process ):
 
-# model.load_weights('04-0.0781')
+    def __init__( self, ip, port ):
+        Process.__init__( self, name = "MachinVisionProcess" )
+
+        print( '[MachinVisionProcess __init__]' )
+
+    def __del__( self ):
+        print( '[MachinVisionProcess __del__]' )
+        self.server_sock.close()
+
+
+
+
+
+
+    def process(img_input):
+        gray = cv2.cvtColor(img_input, cv2.COLOR_BGR2GRAY)
+        gray = cv2.resize(gray, (28, 28), interpolation=cv2.INTER_AREA)
+        (thresh, img_binary) = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+        h,w = img_binary.shape
+
+        ratio = 100/h
+        new_h = 100
+        new_w = w * ratio
+
+        img_empty = np.zeros((110,110), dtype=img_binary.dtype)
+        img_binary = cv2.resize(img_binary, (int(new_w), int(new_h)), interpolation=cv2.INTER_AREA)
+        img_empty[:img_binary.shape[0], :img_binary.shape[1]] = img_binary
+        img_binary = img_empty
+
+        cnts = cv2.findContours(img_binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # 컨투어의 무게중심 좌표를 구합니다. 
+        M = cv2.moments(cnts[0][0])
+        print('{}, {}, {}'.format(M["m00"],M["m10"],M["m01"]))
+        center_x = (M["m10"] / M["m00"])
+        center_y = (M["m01"] / M["m00"])
+
+        # 무게 중심이 이미지 중심으로 오도록 이동시킵니다. 
+        height,width = img_binary.shape[:2]
+        shiftx = width/2-center_x
+        shifty = height/2-center_y
+
+        Translation_Matrix = np.float32([[1, 0, shiftx],[0, 1, shifty]])
+        img_binary = cv2.warpAffine(img_binary, Translation_Matrix, (width,height))
+
+        img_binary = cv2.resize(img_binary, (28, 28), interpolation=cv2.INTER_AREA)
+        flatten = img_binary.flatten() / 255.0
+        return flatten
 
 
 cap = cv2.VideoCapture(1)
@@ -78,7 +66,6 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 model = load_model('04-0.0781.hdf5')  
 
 while 1:
-
     ret, img_color = cap.read()
 
     if ret == False:
@@ -87,9 +74,7 @@ while 1:
     img_input = img_color.copy()
     cv2.rectangle(img_color, (250, 150),  (width-250, height-150), (0, 0, 255), 3)
     cv2.imshow('bgr', img_color)
-
     img_roi = img_input[150:height-150, 250:width-250]
-
 
     key = cv2.waitKey(1)
     try:
@@ -106,7 +91,6 @@ while 1:
     #         cv2.waitKey(0)
     except:
         continue
-
 
 cap.release()
 cv2.destroyAllWindows()
