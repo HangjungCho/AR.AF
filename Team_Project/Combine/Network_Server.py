e48ca47f5cae64dd7fc6d0a506babfff0f2123c1
항정 조 <stneowh@naver.com>
2020-11-18 16:58:02 +0900

    사진 추가

--- Team_Project/Combine/Network_Server.py
+++ Team_Project/Combine/Network_Server.py
@@ -83,25 +83,27 @@ class ClientImgProcess( Process ):
         date = capdate.replace('-', '') + '_' + captime.replace(':', '')
         img_name = date +'.jpg'
 
-        path, dirs, files = next(os.walk("./dataset/train"))
+        path, dirs, files = next(os.walk("./dataset/"+generator_type))
         classification_num = len(dirs)
 
         for dir in dirs:
             if dir[3:] == class_name:
                 dir_flag = 0
                 dir_buf = dir
-        
+
+        print('dir_flag = {}'.format(dir_flag))
         if dir_flag:
-            directory_path = './dataset/'+generator_type+'/'+str(classification_num+1)+'. '+class_name
+            directory_path = './dataset/'+generator_type+'/'+str(classification_num)+'. '+class_name
             if not os.path.exists(directory_path):
                 os.mkdir(directory_path)
         
         else:
             directory_path = './dataset/'+generator_type+'/'+ dir_buf
-        
+            
         img_location = directory_path+'/'+img_name
         print('img_location : {}'.format(img_location))
         cv2.imwrite(img_location, imgdata)
+        
     
     def insertDB( self, imgdata, product):
         conn = sqlite3.connect( 'araf.db' )
@@ -123,7 +125,8 @@ class ClientImgProcess( Process ):
         # save image
         now_date = realdate.replace('-', '') + '_' + realtime.replace(':', '')
         img_name = product_type + '_' + now_date+'.png'
-        img_location = './img_file/'+img_name
+        img_location = './static/img_file/'+img_name
+        db_img = './img_file/'+img_name
         cv2.imwrite(img_location, imgdata)
         web_img_name = 'img/product/' + product_type + '.png'
 
@@ -145,7 +148,7 @@ class ClientImgProcess( Process ):
         error = 0 # 임시로 error을 0으로 두었으나 이건 Machine_Control에서 판명해서 데이터전송을 통해 전달해줘야 하는 정보임.
         # 그러나 제품이 에러가난 경우를 판별하는 기준이 갖추어 있지 않으므로 여기서 임의로 에러가 없다는 0값을 집어넣어준것 뿐임
         
-        data = product_type, cal, count, error, realdate, realtime, img_location
+        data = product_type, cal, count, error, realdate, realtime, db_img
         
 
         with conn:
@@ -153,10 +156,10 @@ class ClientImgProcess( Process ):
             # 에러가 발생할 경우 Count 테이블은 카운트하지 않고 Quantity 테이블의 count항목도 증가하면 안된다.
             if error == 1:
                 cursor.execute( 'UPDATE ErrStat SET num = ? WHERE p_type = ?', (err_cnt[0][0]+1, product_type))
-                cursor.execute( 'INSERT INTO Quantity(p_type, cal, count, error, date, time, img) VALUES(?, ?, ?, ?, ?, ?, ?)', product_type, cal, count, error, realdate, realtime, img_location )
+                cursor.execute( 'INSERT INTO Quantity(p_type, cal, count, error, date, time, img) VALUES(?, ?, ?, ?, ?, ?, ?)', product_type, cal, count, error, realdate, realtime, db_img )
             
             elif error == 0:
-                data = product_type, cal, (count[0][0]+1), error, realdate, realtime, img_location
+                data = product_type, cal, (count[0][0]+1), error, realdate, realtime, db_img
                 print('data : {}'.format(data))
                 # 에러가 발생할 경우 Count 테이블과 Quantity 테이블의 수량 항목이 증가해야 한다.
                 cursor.execute( 'UPDATE  Count SET num = ? WHERE p_type = ? ', (count[0][0]+1, product_type) )
